543c0988a226ff4f638f930a0cff73c5a3d9ecfb
PrabhudevMishra <70210156+PrabhudevMishra@users.noreply.github.com>
2021-05-12 19:00:01 +0530

    Delete App.js

--- App.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import BarCodeScanner from "./screens/barcodeScanner";
-
-export default class App extends React.Component {
-  render() {
-    return <BarCodeScanner />;
-  }
-}
